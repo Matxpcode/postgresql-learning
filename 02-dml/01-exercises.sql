e439a431c3b573ee4cfe8b538ec6fb26a372d582
Matxpcode <mateo.rubiuz@gmail.com>
2026-09-09 20:12:24 -0500

    fix: change value commission_pct for is null

--- 02-dml/01-exercises.sql
+++ 02-dml/01-exercises.sql
@@ -3,6 +3,8 @@
 -- 1. filtrado y expresiones (like y nulls)
 select employee_id, first_name, last_name, salary, commission_pct
 from employees
-where commission_pct=null and salary>=5000 and last_name ilike '%a%';
+where commission_pct is null 
+	and salary>=5000 
+	and last_name ilike '%a%';
 
 
